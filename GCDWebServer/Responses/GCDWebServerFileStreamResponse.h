d037a0d895021ee4062b0ab1cdc292c9cc0aa653
qiudaomao <qiudaomao@gmail.com>
2019-04-08 21:33:38 +0800

    add GCDWebServerFileStreamResponse

---
+++ GCDWebServer/Responses/GCDWebServerFileStreamResponse.h
@@ -0,0 +1,68 @@
+/*
+ Copyright (c) 2012-2019, Pierre-Olivier Latour
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * The name of Pierre-Olivier Latour may not be used to endorse
+ or promote products derived from this software without specific
+ prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL PIERRE-OLIVIER LATOUR BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "GCDWebServerResponse.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+//open and return priv data
+typedef id _Nullable(^openBlock)(void);
+typedef NSInteger(^getSizeBlock)(void);
+typedef BOOL(^seekBlock)(NSInteger);
+typedef NSData* _Nullable(^readBlock)(NSInteger);
+typedef void(^closeBlock)(void);
+
+/**
+ *  The GCDWebServerFileStreamResponse subclass of GCDWebServerResponse reads the body
+ *  of the HTTP response from a file on disk.
+ *
+ *  It will automatically set the contentType, lastModifiedDate and eTag
+ *  properties of the GCDWebServerResponse according to the file extension and
+ *  metadata.
+ */
+@interface GCDWebServerFileStreamResponse : GCDWebServerResponse
+@property(nonatomic, copy) NSString* contentType;  // Redeclare as non-null
+@property(nonatomic) NSDate* lastModifiedDate;  // Redeclare as non-null
+@property(nonatomic, copy) NSString* eTag;  // Redeclare as non-null
+@property(nonatomic, copy) NSString* ext;  // Redeclare as non-null
+@property(nonatomic, copy) NSString* desc;  // Redeclare as non-null
+@property(nonatomic) id privData;
+@property(nonatomic) openBlock onOpen;
+@property(nonatomic) getSizeBlock onGetSize;
+@property(nonatomic) seekBlock onSeek;
+@property(nonatomic) readBlock onRead;
+@property(nonatomic) closeBlock onClose;
+
+- (nullable instancetype)initWithOpenBlock:(openBlock)open
+                              getSizeBlock:(getSizeBlock)getSize
+                                 seekBlock:(seekBlock)seek
+                                 readBlock:(readBlock)read
+                                closeBlock:(closeBlock)close
+                                 extension:(NSString*)ext;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GCDWebServer/Responses/GCDWebServerFileStreamResponse.m
@@ -0,0 +1,103 @@
+/*
+ Copyright (c) 2012-2019, Pierre-Olivier Latour
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * The name of Pierre-Olivier Latour may not be used to endorse
+ or promote products derived from this software without specific
+ prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL PIERRE-OLIVIER LATOUR BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if !__has_feature(objc_arc)
+#error GCDWebServer requires ARC
+#endif
+
+#import <sys/stat.h>
+#import "GCDWebServerFileStreamResponse.h"
+#import "GCDWebServerPrivate.h"
+
+#define kFileReadBufferSize (32 * 1024)
+
+@implementation GCDWebServerFileStreamResponse {
+    NSUInteger _offset;
+    NSUInteger _size;
+}
+
+@dynamic contentType, lastModifiedDate, eTag;
+@synthesize ext;
+@synthesize desc;
+
+- (nullable instancetype)initWithOpenBlock:(openBlock)open
+                              getSizeBlock:(getSizeBlock)getSize
+                                 seekBlock:(seekBlock)seek
+                                 readBlock:(readBlock)read
+                                closeBlock:(closeBlock)close
+                                 extension:(NSString*)ext {
+    id priv = open();
+    if (!priv) return nil;
+    NSInteger size = getSize();
+    if (size<=0) return nil;
+    if ((self = [super init])) {
+        self.onOpen = open;
+        self.onGetSize = getSize;
+        self.onSeek = seek;
+        self.onRead = read;
+        self.onClose = close;
+        self.privData = priv;
+    }
+    _offset = 0;
+    return self;
+}
+
+- (BOOL)open:(NSError**)error {
+    id priv = self.onOpen();
+    if (!priv) return NO;
+    self.privData = priv;
+    NSUInteger size = self.onGetSize();
+    _size = size;
+    self.contentLength = _size;
+    if (_size<=0) return NO;
+    [self setStatusCode:kGCDWebServerHTTPStatusCode_PartialContent];
+    [self setValue:[NSString stringWithFormat:@"bytes %lu-%lu/%lu", (unsigned long)_offset, (unsigned long)(_offset + _size - 1), (unsigned long)size] forAdditionalHeader:@"Content-Range"];
+    self.contentType = GCDWebServerGetMimeTypeForExtension(self.ext, nil);
+    if (self.onSeek(0)) {
+        self.onClose();
+        return NO;
+    }
+    return YES;
+}
+
+- (NSData*)readData:(NSError**)error {
+    size_t length = MIN((NSUInteger)kFileReadBufferSize, _size);
+    NSData *data = self.onRead(length);
+    return data;
+}
+
+- (void)close {
+    self.onClose();
+}
+
+- (NSString*)description {
+  NSMutableString* description = [NSMutableString stringWithString:[super description]];
+  [description appendFormat:@"\n\n{%@}", self.desc];
+  return description;
+}
+
+@end
